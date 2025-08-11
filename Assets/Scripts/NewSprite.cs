1f7ba69076c4c5481eaeb94fe1ef36d04f3bcc6d
MunchiP <paulaviveusar@gmail.com>
2025-08-11 08:06:47 -0500

    modificacion de puntos de salida de oveja, ovejas y sprite de las ovejas

--- Assets/Scripts/NewSprite.cs
+++ Assets/Scripts/NewSprite.cs
@@ -7,23 +7,31 @@ public class NewSprite : MonoBehaviour
      void OnTriggerEnter2D(Collider2D collision)
         {
             PlayerPickUp player = collision.GetComponent<PlayerPickUp>();
-            
-            // Verificar si el jugador existe y lleva un objeto con tag Wool
-            if (player != null && player.carriedObject != null && player.carriedObject.CompareTag("Wool"))
+
+        // Verificar si el jugador existe y lleva un objeto con tag Wool
+        if (player != null && player.carriedObject != null && player.carriedObject.CompareTag("Wool"))
+        {
+            // Cambiar sprite de la oveja
+            SpriteRenderer sr = GetComponent<SpriteRenderer>();
+            if (sr != null)
+                sr.sprite = newSpriteOption;
+
+            // Destruir la lana
+            GameObject triangle = player.carriedObject;
+            player.DropObject(transform.position);
+            Destroy(triangle);
+
+            // Reproducir sonido
+            if (AudioManager.Instance != null && !string.IsNullOrEmpty(sonidoClip))
+                AudioManager.Instance.Play(sonidoClip);
+
+            // codigo para que se relacione con el de Diana y le diga que isWaiting es falsooo
+
+            WaypointNPCMover mover = GetComponent<WaypointNPCMover>();
+            if (mover != null)
             {
-                // Cambiar sprite de la oveja
-                SpriteRenderer sr = GetComponent<SpriteRenderer>();
-                if (sr != null)
-                    sr.sprite = newSpriteOption;
-
-                // Destruir la lana
-                GameObject triangle = player.carriedObject;
-                player.DropObject(transform.position);
-                Destroy(triangle);
-
-                // Reproducir sonido
-                if (AudioManager.Instance != null && !string.IsNullOrEmpty(sonidoClip))
-                    AudioManager.Instance.Play(sonidoClip);
+                mover.IsWaiting = false;
+            }
             }
         }
 
